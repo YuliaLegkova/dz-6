c3bc47ccbc80d6798c7713c985cc85f18802a93b
YuliaLegkova <136889148+YuliaLegkova@users.noreply.github.com>
2024-03-03 23:51:23 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,73 +1,77 @@
-﻿using System.Numerics;
-
-Console.WriteLine("1. Анонимный тип");
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine();
-var venus = new
+﻿internal class Program
 {
-    Name = "Венера",
-    Number = 2,
-    EquatorLength = 38052,
-    PreviousPlanet = (dynamic?)null
-};
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("1. Анонимный тип");
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine();
+        var venus = new
+        {
+            Name = "Венера",
+            Number = 2,
+            EquatorLength = 38052,
+            PreviousPlanet = (dynamic?)null
+        };
 
-Console.WriteLine("Название: " + venus.Name + "\n" +
-    "Номер от солнца: " + venus.Number + "\n" +
-    "Длина экватора: " + venus.EquatorLength + "\n" +
-    "Предыдущая планета: " + (venus.PreviousPlanet == null ? "null" : venus.PreviousPlanet.Name));
-Console.WriteLine("Эквивалентна Венере?: " + venus.Equals(venus));
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine();
+        Console.WriteLine("Название: " + venus.Name + "\n" +
+            "Номер от солнца: " + venus.Number + "\n" +
+            "Длина экватора: " + venus.EquatorLength + "\n" +
+            "Предыдущая планета: " + (venus.PreviousPlanet == null ? "null" : venus.PreviousPlanet.Name));
+        Console.WriteLine("Эквивалентна Венере?: " + venus.Equals(venus));
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine();
 
-var earth = new
-{
-    Name = "Земля",
-    Number = 3,
-    EquatorLength = 40075,
-    PreviousPlanet = venus
-};
+        var earth = new
+        {
+            Name = "Земля",
+            Number = 3,
+            EquatorLength = 40075,
+            PreviousPlanet = venus
+        };
 
-Console.WriteLine("Название: " + earth.Name + "\n" +
-    "Номер от солнца: " + earth.Number + "\n" +
-    "Длина экватора: " + earth.EquatorLength + "\n" +
-    "Предыдущая планета: " + (earth.PreviousPlanet == null ? "null" : earth.PreviousPlanet.Name));
-Console.WriteLine("Эквивалентна Венере?:" + earth.Equals(venus));
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine();
+        Console.WriteLine("Название: " + earth.Name + "\n" +
+            "Номер от солнца: " + earth.Number + "\n" +
+            "Длина экватора: " + earth.EquatorLength + "\n" +
+            "Предыдущая планета: " + (earth.PreviousPlanet == null ? "null" : earth.PreviousPlanet.Name));
+        Console.WriteLine("Эквивалентна Венере?:" + earth.Equals(venus));
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine();
 
-var mars = new
-{
-    Name = "Марс",
-    Number = 4,
-    EquatorLength = 21165,
-    PreviousPlanet = earth
-};
+        var mars = new
+        {
+            Name = "Марс",
+            Number = 4,
+            EquatorLength = 21165,
+            PreviousPlanet = earth
+        };
 
-Console.WriteLine("Название: " + mars.Name + "\n" +
-    "Номер от солнца: " + mars.Number + "\n" +
-    "Длина экватора: " + mars.EquatorLength + "\n" +
-    "Предыдущая планета: " + (mars.PreviousPlanet == null ? "null" : mars.PreviousPlanet.Name));
-Console.WriteLine("Эквивалентна Венере?: " + mars.Equals(venus));
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine();
+        Console.WriteLine("Название: " + mars.Name + "\n" +
+            "Номер от солнца: " + mars.Number + "\n" +
+            "Длина экватора: " + mars.EquatorLength + "\n" +
+            "Предыдущая планета: " + (mars.PreviousPlanet == null ? "null" : mars.PreviousPlanet.Name));
+        Console.WriteLine("Эквивалентна Венере?: " + mars.Equals(venus));
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine();
 
-var venus1 = new
-{
-    Name = "Венера",
-    Number = 2,
-    EquatorLength = 38052,
-    PreviousPlanet = (dynamic?)null
-};
+        var venus1 = new
+        {
+            Name = "Венера",
+            Number = 2,
+            EquatorLength = 38052,
+            PreviousPlanet = (dynamic?)null
+        };
 
-Console.WriteLine("Название: " + venus1.Name + "\n" +
-    "Номер от солнца: " + venus1.Number + "\n" +
-    "Длина экватора: " + venus1.EquatorLength + "\n" +
-    "Предыдущая планета: " + (venus1.PreviousPlanet == null ? "null" : venus1.PreviousPlanet.Name));
-Console.WriteLine("Эквивалентна Венере?: " + venus1.Equals(venus));
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------------");
-Console.WriteLine();
+        Console.WriteLine("Название: " + venus1.Name + "\n" +
+            "Номер от солнца: " + venus1.Number + "\n" +
+            "Длина экватора: " + venus1.EquatorLength + "\n" +
+            "Предыдущая планета: " + (venus1.PreviousPlanet == null ? "null" : venus1.PreviousPlanet.Name));
+        Console.WriteLine("Эквивалентна Венере?: " + venus1.Equals(venus));
+        Console.WriteLine();
+        Console.WriteLine("-----------------------------------------------");
+        Console.WriteLine();
+    }
+}
